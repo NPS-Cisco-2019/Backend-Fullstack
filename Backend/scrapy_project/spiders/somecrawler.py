f492008b8a62e7cca08f930ea9094b46d67ac56c
Student <npsinrcisco@gmail.com>
2019-11-20 14:03:33 +0530

    hello frnds

--- Backend/scrapy_project/spiders/somecrawler.py
+++ Backend/scrapy_project/spiders/somecrawler.py
@@ -77,138 +77,154 @@ class QuotesSpider(scrapy.Spider):
         return self.link_to_be_parsed
 
     def parsebrainly(self, response):
-        ans = response.xpath(
-            "//div[@class='sg-text js-answer-content brn-rich-content']").extract()
+        try:
+            ans = response.xpath(
+                "//div[@class='sg-text js-answer-content brn-rich-content']").extract()
 
-        imgsrc = response.xpath(
-            "//section[@id='answers']//img[@title='Attachment']/@src").getall()
+            imgsrc = response.xpath(
+                "//section[@id='answers']//img[@title='Attachment']/@src").getall()
 
-        ans = self.janitor(str(ans))
+            ans = self.janitor(str(ans))
 
-        self.answer["domain"].append("brainly")
-        self.answer["success"] = 1
-        if imgsrc:
-            self.answer["answer"].append([*ans, imgsrc])
-        else:
-            self.answer["answer"].append([*ans, 0])
-        self.writetheanswer()
+            self.answer["domain"].append("brainly")
+            self.answer["success"] = 1
+            if imgsrc:
+                self.answer["answer"].append([*ans, imgsrc])
+            else:
+                self.answer["answer"].append([*ans, 0])
+            self.writetheanswer(True)
+        except:
+            self.writetheanswer(False)
 
-        # self.log(self.answer)
+            # self.log(self.answer)
 
-    """ def downloadImage(self,urll, urllname):
+        """ def downloadImage(self,urll, urllname):
+            
+            urllib.request.urlretrieve(self.urll, self.urllname) """
+
+        def parseaskiitans(self, response):
+            try:
+                self.log("WWW>ASKIITIANS.COM STARTED")
+                l = str(response.xpath(
+                    '//*[@id="rptAnswers_ctl01_pnlAnswer"]').extract())
+                l = self.janitor(l)
+
+                img = response.xpath(
+                    '//div[@id="rptAnswers_ctl01_pnlAnswer"]//img/@src').extract()
+
+                self.answer["domain"].append("askiitans")
+                self.answer["success"] = 1
+                if img:
+                    self.answer["answer"].append([*l, img])
+                else:
+                    self.answer["answer"].append([*l, 0])
+
+                self.writetheanswer(True)
+            except:
+                self.writetheanswer(False)
         
-        urllib.request.urlretrieve(self.urll, self.urllname) """
-
-    def parseaskiitans(self, response):
-        self.log("WWW>ASKIITIANS.COM STARTED")
-        l = str(response.xpath(
-            '//*[@id="rptAnswers_ctl01_pnlAnswer"]').extract())
-        l = self.janitor(l)
-
-        img = response.xpath(
-            '//div[@id="rptAnswers_ctl01_pnlAnswer"]//img/@src').extract()
-
-        self.answer["domain"].append("askiitans")
-        self.answer["success"] = 1
-        if img:
-            self.answer["answer"].append([*l, img])
-        else:
-            self.answer["answer"].append([*l, 0])
-
-        self.writetheanswer()
 
     def parsestackexchange(self, response):
-        answer = response.xpath("//div[@class='post-text']/p/text()").extract()
-        links = self.convertLinks(response.xpath(
-            "//div[@class='post-text']//a/@href").extract())
-        for i in range(len(answer)):
-            answer[i] = answer[i].replace("$$", "$")
+        try:
+            answer = response.xpath("//div[@class='post-text']/p/text()").extract()
+            links = self.convertLinks(response.xpath(
+                "//div[@class='post-text']//a/@href").extract())
+            for i in range(len(answer)):
+                answer[i] = answer[i].replace("$$", "$")
 
-            # print(f"\n\n\n\n[LINKS] {links}\n\n\n\n")
+                # print(f"\n\n\n\n[LINKS] {links}\n\n\n\n")
 
-        self.answer["answer"].append([*answer, *links, 0])
-        self.answer["domain"].append("Stack Exchange")
-        self.answer["success"] = 1
-        # with open("ans.txt", "w") as f:
-        #     for dom in self.answer["domain"]:
-        #         if dom == "Stack Exchange":
+            self.answer["answer"].append([*answer, *links, 0])
+            self.answer["domain"].append("Stack Exchange")
+            self.answer["success"] = 1
+            # with open("ans.txt", "w") as f:
+            #     for dom in self.answer["domain"]:
+            #         if dom == "Stack Exchange":
 
-        self.writetheanswer()
+            self.writetheanswer(True)
+        except:
+            self.writetheanswer(False)
 
     def parsedoubtnut(self, response):
-        self.log("D FOR DOUBTNUT")
+        try:
+            self.log("D FOR DOUBTNUT")
 
-        htmls = str(response.text)
-        i_ans_text = htmls.find("Answer Text")
+            htmls = str(response.text)
+            i_ans_text = htmls.find("Answer Text")
 
-        htmls = htmls[i_ans_text:]
+            htmls = htmls[i_ans_text:]
 
-        p_indexes = [m.start() for m in re.finditer('</p>', htmls)][2]
-        half_almost_answer = htmls[:p_indexes]
-        ind_almost_answer = half_almost_answer[::-1].find('>')
-        answer = half_almost_answer[-ind_almost_answer:]
-        answer = self.janitor(answer)
-        self.answer["success"] = 1
+            p_indexes = [m.start() for m in re.finditer('</p>', htmls)][2]
+            half_almost_answer = htmls[:p_indexes]
+            ind_almost_answer = half_almost_answer[::-1].find('>')
+            answer = half_almost_answer[-ind_almost_answer:]
+            answer = self.janitor(answer)
+            self.answer["success"] = 1
 
-        self.answer["domain"].append("doubtnut")
-        self.answer["answer"].append([*answer, 0])
-        self.writetheanswer()
+            self.answer["domain"].append("doubtnut")
+            self.answer["answer"].append([*answer, 0])
+            self.writetheanswer(True)
+        except:
+            self.writetheanswer(False)
 
     def janitor(self, raw_html):
-        split_str = '!@#$%^&*()'
+        try:
+            split_str = '!@#$%^&*()'
 
-        # self.log(str(raw_html))
+            # self.log(str(raw_html))
 
-        # cleanr = re.compile('<.*?>')
-        # self.log("[]")
-        # self.log("[UNCLEANED TEXT]")
-        # self.log(str(cleanr))
-        cleantext = re.sub('<br/?>', split_str, raw_html)
-        cleantext = re.sub('&lt;br&gt;', split_str, cleantext)
-        cleantext = re.sub('<p.*?>', split_str, cleantext)
-        cleantext = re.sub('&gt;', '>', cleantext)
-        cleantext = re.sub('<.*?>', ' ', cleantext)
-        cleantext = re.sub('\\\\xa0', ' ', cleantext)
+            # cleanr = re.compile('<.*?>')
+            # self.log("[]")
+            # self.log("[UNCLEANED TEXT]")
+            # self.log(str(cleanr))
+            cleantext = re.sub('<br/?>', split_str, raw_html)
+            cleantext = re.sub('&lt;br&gt;', split_str, cleantext)
+            cleantext = re.sub('<p.*?>', split_str, cleantext)
+            cleantext = re.sub('&gt;', '>', cleantext)
+            cleantext = re.sub('<.*?>', ' ', cleantext)
+            cleantext = re.sub('\\\\xa0', ' ', cleantext)
 
-        cleantext = re.sub('?\\\\[A-Za-z]? ', ' ', cleantext)
+            cleantext = re.sub('\\\\[A-Za-z]', ' ', cleantext)
 
-        self.log("[DATA]")
+            self.log("[DATA]")
+            
+            
+            
+            cleantext = str(cleantext).rstrip(" []'")
+            cleantext = str(cleantext).lstrip("[] '")
+            i = 0
+            while i < len(cleantext) -1 :
+                if cleantext[i] == '$' and cleantext[i+1] != "$"  :
+                    cleantext = cleantext[:i] + "$$" + cleantext[i+1:]
+                    i+=2
+                    self.log("[WORKS]")
+                i+=1
+            i = 0
+            spaceFound = False
+            while i < len(cleantext):
+
+                if cleantext[i] == " " and spaceFound == False:
+                    temp = i
+                    spaceFound = True
+                if cleantext[i] != " " and spaceFound == True:
+                    spaceFound = False
+                    cleantext = cleantext[:temp] + " " + cleantext[i:]
+                i += 1
+
+            self.log("[CLEANED TEXT]")
+
         
-        
-        
-        cleantext = str(cleantext).rstrip(" []'")
-        cleantext = str(cleantext).lstrip("[] '")
-        i = 0
-        while i < len(cleantext) -1 :
-            if cleantext[i] == '$' and cleantext[i+1] != "$"  :
-                cleantext = cleantext[:i] + "$$" + cleantext[i+1:]
-                i+=2
-                self.log("[WORKS]")
-            i+=1
-        i = 0
-        spaceFound = False
-        while i < len(cleantext):
+            # l = cleantext.split(split_str)
+            # i = 0
+            
+            # while i < len(l) :
+            #     if l[i] == ' ' or l[i] == '' :
+            #         l.pop(i)
+            #     i += 1
 
-            if cleantext[i] == " " and spaceFound == False:
-                temp = i
-                spaceFound = True
-            if cleantext[i] != " " and spaceFound == True:
-                spaceFound = False
-                cleantext = cleantext[:temp] + " " + cleantext[i:]
-            i += 1
-
-        self.log("[CLEANED TEXT]")
-
-       
-        # l = cleantext.split(split_str)
-        i = 0
-        
-        while i < len(l) :
-            if l[i] == ' ' or l[i] == '' :
-                l.pop(i)
-            i += 1
-
-        return l
+            return cleantext
+        except :
+            self.writetheanswer(False)
 
     def convertLinks(self, links):
         newLinks = []
@@ -219,8 +235,15 @@ class QuotesSpider(scrapy.Spider):
 
         return newLinks
 
-    def writetheanswer(self):
-        a = open("ans.txt", "a+")
-        a.close()
-        with open("ans.txt", "w") as f:
-            f.write(str(self.answer))
+    def writetheanswer(self,works):
+        if works:
+            a = open("ans.txt", "a+")
+            a.close()
+            with open("ans.txt", "w") as f:
+                f.write(str(self.answer))
+        else:
+            self.answer = {"answer": ['Couldn\'t fetch answer, please try again'], "domain": ['Error'], "success": [0]}
+            a = open("ans.txt", "a+")
+            a.close()
+            with open("ans.txt", "w") as f:
+                f.write(str(self.answer))
