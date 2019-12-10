a1477f934789490d3abef55e2bcc22ef8ccd01ef
Student <npsinrcisco@gmail.com>
2019-12-10 11:01:23 +0530

    TODO

--- Backend/OCR/config.py
+++ Backend/OCR/config.py
@@ -1,15 +1,19 @@
 import numpy as numpy
 import cv2 as cv
 import pytesseract
-import sys, base64, os
+import sys
+import base64
+import os
 import time
 
 TESTING = False
 p_thresh = 0.001
 
+
 def fileLog(*vals):
-    with open('OCR_progress_log.txt', 'a+') as f: 
+    with open('OCR_progress_log.txt', 'a+') as f:
         f.write(''.join(str(elem) for elem in vals) + '\n')
+
 
 def log_error(*args):
     s = "[" + time.strftime("%d-%m %H:%M:%S") + " IST]   "
@@ -22,6 +26,7 @@ def log_error(*args):
         with open(os.path.join(os.getcwd(), "error_logs", "ocr.log"), "a+") as f:
             f.write(s)
 
+
 if TESTING:
     log = print
 else:
@@ -31,7 +36,8 @@ else:
 def initLog():
     open('OCR_progress_log.txt', 'w').close()
 
+
 sys.path.append(os.path.join(os.getcwd(), "OCR"))
 
 # NOTE Uncomment if running on windows
-# pytesseract.pytesseract.tesseract_cmd = r"C:\Users\Student\AppData\Local\Tesseract-OCR\tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = r"C:\Users\Student\AppData\Local\Tesseract-OCR\tesseract.exe"
