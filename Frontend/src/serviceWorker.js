8af6760d88aea2f683a6ee81faf27220f959fbb7
Lutetium-Vanadium <luv.s7000@gmail.com>
2020-01-25 14:30:05 +0530

    Updated serviceWorker

--- Frontend/src/serviceWorker.js
+++ Frontend/src/serviceWorker.js
@@ -57,7 +57,7 @@ export function register(config) {
 
 function registerValidSW(swUrl, config) {
   navigator.serviceWorker
-    .register(swUrl, {scope: "/"})
+    .register(swUrl, { scope: "/" })
     .then(registration => {
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
